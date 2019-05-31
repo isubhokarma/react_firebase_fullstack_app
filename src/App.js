f344151df1568601b6cbbc6a65f61c330b0b7765
Subho Karmakar <isubhokarma@gmail.com>
2019-05-31 17:59:35 +0530

    Nav/ SignedIn/ SignedOut

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <h1>Hello, World!</h1>
+        <Navbar />
       </div>
     </BrowserRouter>
   );
