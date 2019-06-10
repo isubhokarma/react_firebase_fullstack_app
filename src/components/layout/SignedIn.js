26db3ffdd2b20ca9496923abde19940435facaa5
Subho Karmakar <isubhokarma@gmail.com>
2019-06-10 09:42:08 +0530

    Firebase Auth -- Logout

--- src/components/layout/SignedIn.js
+++ src/components/layout/SignedIn.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { signOut } from "../../store/actions/authActions";
 
-const SignedIn = () => {
+const SignedIn = props => {
   return (
     <ul className="right">
       <li>
         <NavLink to="/create">New Project</NavLink>
       </li>
       <li>
-        <NavLink to="/">Log Out</NavLink>
+        <a onClick={props.signOut}>Log Out</a>
       </li>
       <li>
         <NavLink to="/" className="btn btn-floating red lighten-1">
@@ -19,4 +21,13 @@ const SignedIn = () => {
   );
 };
 
-export default SignedIn;
+const mapDispatchToProps = dispatch => {
+  return {
+    signOut: () => dispatch(signOut())
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SignedIn);
